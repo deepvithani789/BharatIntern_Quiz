83e485d027338036618cf1f211fb7e49d71a9deb
Deep Vithani <deepvithani789@gmail.com>
2024-06-07 15:01:51 +0530

    Q

---
+++ app/src/main/java/com/example/myquiz/QuestionAnswer.java
@@ -0,0 +1,43 @@
+package com.example.myquiz;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "Which Player has record of 600 sixes in International Cricket ?",
+            "Which Player is known as the 'Chase Master' in Cricket ?",
+            "Which Team won the Inaurgal ICC T20 World Cup started in 2007 ?",
+            "Who was the Player of Tournament in ICC Cricket World Cup 2011 ?",
+            "Which Player is known as 'The Alien' or 'Mr.360°' in International Cricket ?",
+            "Which Team has played both World Test Championship Finals in 2021 and 2023 ?",
+            "Which Player has not bowled a No-Ball in T20 International Cricket ?",
+            "Which Player is famous for his Rocket Throw in International Cricket0",
+            "Border-Gavaskar Trophy is Played between which Teams ?",
+            "The Ashes is Played between which Teams ?"
+    };
+
+    public static String choices[][] ={
+            {"Virat Kohli","Chris Gayle","Rohit Sharma","Glenn Maxwell"},
+            {"Rohit Sharma","Virat Kohli","Babar Azam","David Warner"},
+            {"India","Australia","England","West Indies"},
+            {"MS Dhoni","Zaheer Khan","Sachin Tendulkar","Yuvraj Singh"},
+            {"Kane Williamson","ABD Villiers","Glenn Maxwell","Jos Buttler"},
+            {"New Zealand","Australia","India","England"},
+            {"Bhuvneshwar Kumar","Dale Steyn","Mitchell Starc","Trent Boult"},
+            {"Virat Kohli","Jhonty Rodes","Martin Guptill","Ravindra Jadeja"},
+            {"Australia vs England","India vs Australia","England vs India","New Zealand vs Australia"},
+            {"England vs India","England vs South Africa","Australia vs England","Australia vs India"}
+    };
+
+    public static String correctAnswers[] ={
+            "Rohit Sharma",
+            "Virat Kohli",
+            "India",
+            "Yuvraj Singh",
+            "ABD Villiers",
+            "India",
+            "Bhuvneshwar Kumar",
+            "Ravindra Jadeja",
+            "India vs Australia",
+            "Australia vs England"
+    };
+}
